7404e244667719decb0c09569779e422f4afdb3e
Ahmed Asadov <zoxon470@gmail.com>
2017-11-11 19:20:33 +0600

    Added app jiro

---
+++ users_service/zoxon470/jiro/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class JiroProfile(models.Model):
+    username = models.CharField(max_length=125, verbose_name='Логин от Jira')
+    hours_per_day = models.IntegerField(verbose_name='Количество часов за день')
+    hours_per_week = models.IntegerField(verbose_name='Количество часов за неделю')
+    hours_per_month = models.IntegerField(verbose_name='Количество часов за месяц')
+
+    def __str__(self):
+        return self.username
