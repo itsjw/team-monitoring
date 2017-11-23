050fc2507ca3e24a384e36e9592bfdc7328cf8e3
Ekaterina Premudraya <smolyaevaekaterina@gmail.com>
2017-11-23 19:06:06 +0300

    gitlab views added

--- users_service/zoxon470/gitlab/views.py
+++ users_service/zoxon470/gitlab/views.py
@@ -3,95 +3,74 @@
 import os
 import requests
 
-from jira import JIRA
-from requests.auth import HTTPBasicAuth
-from datetime import datetime
+from datetime import datetime, timedelta
 
-JIRA_USERNAME = os.environ.get('JIRA_USERNAME')
-JIRA_PASSWORD = os.environ.get('JIRA_PASSWORD')
-JIRA_ASSIGNEE = os.environ.get('JIRA_ASSIGNEE')
+from urllib3.connectionpool import xrange
 
-
-# autentification per jiralib and api
-jira = JIRA(basic_auth=(JIRA_USERNAME, JIRA_PASSWORD), options={'server': 'https://pm.maddevs.co/'})
-auth = HTTPBasicAuth(username=JIRA_USERNAME, password=JIRA_PASSWORD)
-current_day = datetime.today().strftime("%Y-%m-%d")
+GITLAB_TOKEN = os.environ.get('GITLAB_TOKEN')
+GITLAB_ASSIGNEE = os.environ.get('GITLAB_ASSIGNEE')
 
 
-class JIRATimeSpent():
-    print('class')
-    def time_tracked_per_day(self):
-        print("Grabbing user info for {}".format(JIRA_ASSIGNEE))
-        # getting all issues updated by user since start of the day
-        issues = []
-        search_categorys = 'assignee = {0} AND  updated  > startOfDay()'.format(JIRA_ASSIGNEE)
-        all_proj_issues_by_assignee = jira.search_issues(search_categorys)
-        for key in all_proj_issues_by_assignee:
-            issues.append(key)
+class GitlabCommitTracked():
+    def __init__(self, user_id):
+        self.user_id = user_id
 
-            # getting all worklog info per person
-            for issue in issues:
-                worklog_url = 'https://pm.maddevs.co/rest/api/2/issue/{0}/worklog'.format(issue)
-                worklog_info = requests.get(worklog_url, auth=auth).json()
+    def getting_user_id(self):
+        print("Grabbing user info for {}".format(GITLAB_ASSIGNEE))
+        resp = requests.get("https://gitlab.com/api/v4/users?username={}".format(GITLAB_ASSIGNEE))
+        user_id = resp.json()[0].get('id', 0)
+        if user_id == 0:
+            return "user {} not found".format(GITLAB_ASSIGNEE)
+        return self.user_id
 
-                # getting updated time of issue and adding them all
-                all_issues_per_day = 0
-                for item in worklog_info['worklogs']:
-                    time_per_track = item['timeSpentSeconds']
-                    all_issues_per_day += time_per_track
-                    return all_issues_per_day
+    def get_commits_per_day(self):
+        print("Grabbing events data...")
+        params = {
+            "private_token": GITLAB_TOKEN,
+            "before": datetime.now().strftime("%Y-%m-%d"),
+            "after": (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d"),
+        }
+        r = requests.get("https://gitlab.com/api/v4/users/{}/events".format(self.user_id), params=params)
+        messages = []
+        for i in xrange(7):
+            day = datetime.now() - timedelta(i)
+            commits = 0
+            for event in r.json():
+                commit_date = datetime.strptime(event.get("created_at"), "%Y-%m-%dT%H:%M:%S.%fZ")
+                if (day - commit_date).days == 0:
+                    commits += 1
 
-    def time_tracked_per_week(self):
-        print("Grabbing user info for {}".format(JIRA_ASSIGNEE))
-        issues = []
-        search_categorys = 'assignee = {0} AND  updated  <=  endOfWeek()'.format(JIRA_ASSIGNEE)
 
-        all_proj_issues_by_assignee = jira.search_issues(search_categorys)
-        print(all_proj_issues_by_assignee)
-        for key, value in all_proj_issues_by_assignee.items():
-            print("KEY - %s VALUE - %s" % (key, value))
-            #print(key)
-            issues.append(key)
-            #print(issues)
+    def get_commits_per_week(self):
+        print("Grabbing events data...")
+        params = {
+            "private_token": GITLAB_TOKEN,
+            "before": datetime.now().strftime("%Y-%m-%d"),
+            "after": (datetime.now() - timedelta(days=7)).strftime("%Y-%m-%d"),
+        }
+        r = requests.get("https://gitlab.com/api/v4/users/{}/events".format(self.user_id), params=params)
+        messages = []
+        for i in xrange(7):
+            day = datetime.now() - timedelta(i)
+            commits = 0
+            for event in r.json():
+                commit_date = datetime.strptime(event.get("created_at"), "%Y-%m-%dT%H:%M:%S.%fZ")
+                if (day - commit_date).days == 0:
+                    commits += 1
 
-            # getting all worklog info per person
-            for issue in issues:
-                worklog_url = 'https://pm.maddevs.co/rest/api/2/issue/{0}/worklog'.format(issue)
-                worklog_info = requests.get(worklog_url, auth=auth).json()
-                # print (worklog_url)
-
-                # getting updated time of issue and adding them all
-                all_issues_per_week = 0
-                for item in worklog_info['worklogs']:
-                    # print(item)
-                    time_per_track = item['timeSpentSeconds']
-                    all_issues_per_week += time_per_track
-                    print(all_issues_per_week)
-                    return all_issues_per_week
-
-    def time_tracked_per_month(self):
-        print("Grabbing user info for {}".format(JIRA_ASSIGNEE))
-        issues = []
-        search_categorys = 'assignee = {0} AND  updated  <=  endOfMonth()'.format(JIRA_ASSIGNEE)
-        all_proj_issues_by_assignee = jira.search_issues(search_categorys)
-        print(type(all_proj_issues_by_assignee))
-        print(all_proj_issues_by_assignee)
-        for key in all_proj_issues_by_assignee:
-            print(key)
-            issues.append(key)
-            print(issues)
-
-            # getting all worklog info per person
-            for issue in issues:
-                worklog_url = 'https://pm.maddevs.co/rest/api/2/issue/{0}/worklog'.format(issue)
-                worklog_info = requests.get(worklog_url, auth=auth).json()
-                # print (worklog_url)
-
-                # getting updated time of issue and adding them all
-                all_issues_per_month = 0
-                for item in worklog_info['worklogs']:
-                    # print(item)
-                    time_per_track = item['timeSpentSeconds']
-                    all_issues_per_month += time_per_track
-                    print(all_issues_per_month)
-                    return all_issues_per_month
+    def time_commits_per_month(self):
+        print("Grabbing events data...")
+        params = {
+            "private_token": GITLAB_TOKEN,
+            "before": datetime.now().strftime("%Y-%m-%d"),
+            "after": (datetime.now() - timedelta(days=30)).strftime("%Y-%m-%d"),
+        }
+        r = requests.get("https://gitlab.com/api/v4/users/{}/events".format(self.user_id), params=params)
+        messages = []
+        for i in xrange(7):
+            day = datetime.now() - timedelta(i)
+            commits = 0
+            for event in r.json():
+                commit_date = datetime.strptime(event.get("created_at"), "%Y-%m-%dT%H:%M:%S.%fZ")
+                if (day - commit_date).days == 0:
+                    commits += 1
