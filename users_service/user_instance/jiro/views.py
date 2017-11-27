4b16a5d7b31b8537c4c84b1a0373cf1615a35fab
Ekaterina Premudraya <smolyaevaekaterina@gmail.com>
2017-11-27 21:37:58 +0300

    #5 total logic refactoring

--- users_service/user_instance/jiro/views.py
+++ users_service/user_instance/jiro/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import json
 import os
 import requests
 
@@ -7,77 +7,98 @@ from jira import JIRA
 from requests.auth import HTTPBasicAuth
 from datetime import datetime
 
-JIRA_USERNAME = os.environ.get('JIRA_USERNAME')
-JIRA_PASSWORD = os.environ.get('JIRA_PASSWORD')
-JIRA_ASSIGNEE = os.environ.get('JIRA_ASSIGNEE')
 
+class JIRATimeSpent():
+    def __init__(self):
+        self.JIRA_USERNAME = os.environ.get('JIRA_USERNAME')
+        self.JIRA_PASSWORD = os.environ.get('JIRA_PASSWORD')
+        self.JIRA_ASSIGNEE = os.environ.get('JIRA_ASSIGNEE')
+        self.SERVER = 'https://pm.maddevs.co/'
 
-# autentification per jiralib and api
-jira = JIRA(basic_auth=(JIRA_USERNAME, JIRA_PASSWORD), options={'server': 'https://pm.maddevs.co/'})
-auth = HTTPBasicAuth(username=JIRA_USERNAME, password=JIRA_PASSWORD)
-current_day = datetime.today().strftime("%Y-%m-%d")
-
-
-class JIRATimeSpent(JIRA_USERNAME, JIRA_PASSWORD, JIRA_ASSIGNEE):
+        # autentification per jiralib and api
+        self.jira_api_auth = JIRA(basic_auth=(self.JIRA_USERNAME, self.JIRA_PASSWORD), options={'server': self.SERVER})
+        self.jira_http_auth = HTTPBasicAuth(username=self.JIRA_USERNAME, password=self.JIRA_PASSWORD)
+        self.current_day = datetime.today().strftime("%Y-%m-%d")
 
     def time_tracked_per_day(self):
-        print("Grabbing user info for {}".format(JIRA_ASSIGNEE))
+        print("Grabbing user info for {}".format(self.JIRA_ASSIGNEE))
         # getting all issues updated by user since start of the day
-        issues = []
-        search_categorys = 'assignee = {0} AND  updated  > startOfDay()'.format(JIRA_ASSIGNEE)
-        all_proj_issues_by_assignee = jira.search_issues(search_categorys)
-        for key in all_proj_issues_by_assignee:
-            issues.append(key)
+        search_categorys = 'worklogAuthor = {0} AND worklogDate >= startOfDay()'.format(self.JIRA_ASSIGNEE)
+        all_proj_issues_by_assignee = self.jira_api_auth.search_issues(search_categorys)
+        keys = sorted([project.key for project in all_proj_issues_by_assignee])
+        if not keys:
+            return "there are no issues for today"
 
-            # getting all worklog info per person
-            for issue in issues:
-                worklog_url = 'https://pm.maddevs.co/rest/api/2/issue/{0}/worklog'.format(issue)
-                worklog_info = requests.get(worklog_url, auth=auth).json()
+        # getting all worklog info per person
+        for issue in keys:
+            print("hmm, issue!")
+            worklog_url = 'https://pm.maddevs.co/rest/api/2/issue/{0}/worklog'.format(issue)
+            worklog_info = requests.get(worklog_url, auth=self.jira_http_auth).json()
 
-                # getting updated time of issue and adding them all
-                all_issues_per_day = 0
-                for item in worklog_info['worklogs']:
+            # getting updated time of issue and adding them all
+            all_issues_per_day = []
+            for item in worklog_info['worklogs']:
+                print('one more time log')
+                item_format = "%Y-%m-%dT%H:%M:%S.%f+0000"
+                worklog_date = datetime.strptime(item['updated'], item_format).strftime("%Y-%m-%d")
+                if worklog_date == self.current_day:
                     time_per_track = item['timeSpentSeconds']
-                    all_issues_per_day += time_per_track
-                    return all_issues_per_day
+                    all_issues_per_day.append(time_per_track)
+
+            return all_issues_per_day
 
     def time_tracked_per_week(self):
-        print("Grabbing user info for {}".format(JIRA_ASSIGNEE))
+        print("Grabbing user info for {}".format(self.JIRA_ASSIGNEE))
         issues = []
-        search_categorys = 'assignee = {0} AND  updated  <=  endOfWeek()'.format(JIRA_ASSIGNEE)
-        all_proj_issues_by_assignee = jira.search_issues(search_categorys)
-        for key in all_proj_issues_by_assignee:
-            issues.append(key)
+        search_categorys = 'worklogAuthor = {0} AND worklogDate <=  endOfWeek()'.format(self.JIRA_ASSIGNEE)
+        all_proj_issues_by_assignee = self.jira_api_auth.search_issues(search_categorys)
+        keys = sorted([project.key for project in all_proj_issues_by_assignee])
+        if not keys:
+            return "there are no issues for this week"
 
-            # getting all worklog info per person
-            for issue in issues:
-                worklog_url = 'https://pm.maddevs.co/rest/api/2/issue/{0}/worklog'.format(issue)
-                worklog_info = requests.get(worklog_url, auth=auth).json()
+        # getting all worklog info per person
+        for issue in keys:
+            print("hmm, issue!")
+            worklog_url = 'https://pm.maddevs.co/rest/api/2/issue/{0}/worklog'.format(issue)
+            worklog_info = requests.get(worklog_url, auth=self.jira_http_auth).json()
 
-                # getting updated time of issue and adding them all
-                all_issues_per_week = 0
-                for item in worklog_info['worklogs']:
+            # getting updated time of issue and adding them all
+            all_issues_per_week = []
+            for item in worklog_info['worklogs']:
+                print('one more time log')
+                item_format = "%Y-%m-%dT%H:%M:%S.%f+0000"
+                worklog_date = datetime.strptime(item['updated'], item_format).strftime("%Y-%m-%d")
+                if worklog_date == self.current_day:
+
                     time_per_track = item['timeSpentSeconds']
-                    all_issues_per_week += time_per_track
-                    return all_issues_per_week
+                    all_issues_per_week.append(time_per_track)
+
+            return all_issues_per_week
+
 
     def time_tracked_per_month(self):
-        print("Grabbing user info for {}".format(JIRA_ASSIGNEE))
+        print("Grabbing user info for {}".format(self.JIRA_ASSIGNEE))
         issues = []
-        search_categorys = 'assignee = {0} AND  updated  <=  endOfMonth()'.format(JIRA_ASSIGNEE)
-        all_proj_issues_by_assignee = jira.search_issues(search_categorys)
-        for key in all_proj_issues_by_assignee:
-            issues.append(key)
+        search_categorys = 'worklogAuthor = {0} AND worklogDate  <=  endOfMonth()'.format(self.JIRA_ASSIGNEE)
+        all_proj_issues_by_assignee = self.jira_api_auth.search_issues(search_categorys)
+        keys = sorted([project.key for project in all_proj_issues_by_assignee])
+        if not keys:
+            return "there are no issues for this month"
 
-            # getting all worklog info per person
-            for issue in issues:
-                worklog_url = 'https://pm.maddevs.co/rest/api/2/issue/{0}/worklog'.format(issue)
-                worklog_info = requests.get(worklog_url, auth=auth).json()
+        # getting all worklog info per person
+        for issue in keys:
+            print("hmm, issue!")
+            worklog_url = 'https://pm.maddevs.co/rest/api/2/issue/{0}/worklog'.format(issue)
+            worklog_info = requests.get(worklog_url, auth=self.jira_http_auth).json()
 
-
-                # getting updated time of issue and adding them all
-                all_issues_per_month = 0
-                for item in worklog_info['worklogs']:
+            # getting updated time of issue and adding them all
+            all_issues_per_day = []
+            for item in worklog_info['worklogs']:
+                print('one more time log')
+                item_format = "%Y-%m-%dT%H:%M:%S.%f+0000"
+                worklog_date = datetime.strptime(item['updated'], item_format).strftime("%Y-%m-%d")
+                if worklog_date == self.current_day:
                     time_per_track = item['timeSpentSeconds']
-                    all_issues_per_month += time_per_track
-                    return all_issues_per_month
+                    all_issues_per_day.append(time_per_track)
+
+            return all_issues_per_day
