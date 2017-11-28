3ba79359ddc1a13c7ec575a8d19fd8eb2efa5e94
Ahmed <zoxon470@gmail.com>
2017-11-28 22:29:33 +0600

    Created GitlabProfile model

--- user_service/employee_one/gitlab/models.py
+++ user_service/employee_one/gitlab/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class GitLabProfile(models.Model):
+    username = models.CharField(max_length=125, verbose_name='Username from Gitlab')
+    commits_per_day = models.IntegerField(verbose_name='Commits per day')
+    commits_per_week = models.IntegerField(verbose_name='Commits per week')
+    commits_per_month = models.IntegerField(verbose_name='Commits per month')
+
+    def __str__(self):
+        return self.username
