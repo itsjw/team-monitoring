3e6515537cefb85383613f16e1407d11a2b363c5
Ahmed <zoxon470@gmail.com>
2017-11-28 23:16:51 +0600

    Added Employee model

--- user_service/employee_one/api/models.py
+++ user_service/employee_one/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from jira.models import JiraProfile
+from gitlab.models import GitLabProfile
 
-# Create your models here.
+
+class Employee(models.Model):
+    avatar_url = models.CharField(max_length=255, verbose_name='Avatar')
+    jira = models.ForeignKey(JiraProfile, verbose_name='Jira', default=1)
+    gitlab = models.ForeignKey(GitLabProfile, verbose_name='GitLab', default=1)
+
+    def __str__(self):
+        return 'Employee'
