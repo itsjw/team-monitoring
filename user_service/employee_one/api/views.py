c090c725e69bcd64769e4e3a2a860a3fe9061011
Ahmed <zoxon470@gmail.com>
2017-11-29 00:14:16 +0600

    Created views

--- user_service/employee_one/api/views.py
+++ user_service/employee_one/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from jira.models import JiraProfile
+from gitlab.models import GitLabProfile
+from api.models import Employee
+from api.serializers import JiraProfileSerializer, GitLabProfileSerializer, EmployeeSerializer
 
-# Create your views here.
+
+class JiraProfileViewSet(viewsets.ModelViewSet):
+    queryset = JiraProfile.objects.all()
+    serializer_class = JiraProfileSerializer
+
+
+class GitLabProfileViewSet(viewsets.ModelViewSet):
+    queryset = GitLabProfile.objects.all()
+    serializer_class = GitLabProfileSerializer
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()[:1]
+    serializer_class = EmployeeSerializer
