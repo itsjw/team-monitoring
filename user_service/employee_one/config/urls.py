b50a3635602ecea626acd028f4e1ae02f3148c13
Ahmed <zoxon470@gmail.com>
2017-11-29 00:16:26 +0600

    Included api urls

--- user_service/employee_one/config/urls.py
+++ user_service/employee_one/config/urls.py
@@ -8,7 +8,7 @@ from django.views import defaults as default_views
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
-
+    url(r'^api/v1/', include('api.urls', namespace='api-v1')),
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
 
